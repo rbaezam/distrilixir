ec26c6807b32279b5bc37334012cb25ceb08b757
Rodolfo Baeza <rbaezam@gmail.com>
2021-11-18 22:30:12 -0600

    Added initial tests for inventory module

---
+++ test/distrilixir/inventory_test.exs
@@ -0,0 +1,9 @@
+defmodule Distrilixir.InventoryTest do
+  use Distrilixir.DataCase
+
+  alias Distrilixir.Inventory
+
+  # import Distrilixir.InventoryFixtures
+  alias Distrilixir.Inventory.{Category}
+
+end
